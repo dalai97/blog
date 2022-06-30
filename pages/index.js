dd8bd6241a8553994b821b4cdcb9255f5042cd59
erdenedalai <eska0923@gmail.com>
2022-06-30 15:37:23 +0800

    react bootstarap oruulj irlee

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
 import styles from "../styles/Home.module.css";
-
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
 export default function Home() {
-  return <div className={styles.container}>Миний блог</div>;
+  return (
+    <div className={styles.container}>
+      <Row className="mx-0">
+        <Button as={Col} variant="primary">
+          Button #1
+        </Button>
+        <Button as={Col} variant="secondary" className="mx-2">
+          Button #2
+        </Button>
+        <Button as={Col} variant="danger">
+          Button #3
+        </Button>
+      </Row>
+    </div>
+  );
 }
